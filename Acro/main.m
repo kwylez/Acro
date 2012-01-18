3e2dda02ae67600022ddb9947088f0ed6325ff14
Cory D. Wiles <kwylez@gmail.com>
2012-01-18 14:52:16 -0500

    Initial Commit

---
+++ Acro/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Acro
+//
+//  Created by Cory D. Wiles on 1/18/12.
+//  Copyright (c) 2012 VW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
